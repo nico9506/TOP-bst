2a0ad958a78c3955247b64d9ac5b78351640a7f7
Nicolas Lopez <niclopezt@gmail.com>
2024-05-19 21:59:42 +1000

    Create Tree class including buildTree and print functions

---
+++ Tree.js
@@ -0,0 +1,93 @@
+import { Node } from "./Node";
+
+export const Tree = class {
+    /**
+     * Binary Tree Data Structure
+     * @param {Array} bar - foo
+     */
+
+    #root;
+
+    constructor(inputArray) {
+        this.#root = this.buildTree(inputArray);
+    }
+
+    //Utils
+    buildTree(inputArray) {
+        /**
+         * A function that constructs Balanced Binary Search Tree from
+         * the given numeric array
+         * Taken from:
+         * https://www.geeksforgeeks.org/sorted-array-to-balanced-bst/
+         */
+
+        if (inputArray.length === 0) return null;
+
+        const sortedArray = Array.from(
+            new Set(inputArray.sort((a, b) => a - b))
+        );
+
+        const mid = Math.floor(sortedArray.length / 2);
+        const root = new Node(sortedArray[mid]);
+
+        // Init queue
+        const q = [
+            [root, [0, mid - 1]],
+            [root, [mid + 1, sortedArray.length - 1]],
+        ];
+
+        while (q.length > 0) {
+            const [parent, [left, right]] = q.shift();
+
+            // if there are elements to process and parent node is not NULL
+            if (left <= right && parent != null) {
+                const mid = Math.floor((left + right) / 2);
+                const child = new Node(sortedArray[mid]);
+
+                // set the child node as left or right child of the parent node
+                if (sortedArray[mid] < parent.data) {
+                    parent.leftChild = child;
+                } else {
+                    parent.rightChild = child;
+                }
+
+                // push the left and right child and their indices to the queue
+                q.push([child, [left, mid - 1]]);
+                q.push([child, [mid + 1, right]]);
+            }
+        }
+
+        return root;
+    }
+
+    prettyPrint(node, prefix = "", isLeft = true) {
+        /**
+         * Taken from:
+         * theodinproject.com/lessons/javascript-binary-search-trees#project-solution
+         */
+
+        if (node === null) {
+            return;
+        }
+        if (node.right !== null) {
+            prettyPrint(
+                node.right,
+                `${prefix}${isLeft ? "│   " : "    "}`,
+                false
+            );
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+            prettyPrint(
+                node.left,
+                `${prefix}${isLeft ? "    " : "│   "}`,
+                true
+            );
+        }
+    }
+
+    // Getters and Setters
+    get root() {
+        return this.#root;
+    }
+};
